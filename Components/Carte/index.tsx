2e83cade40da80309864cff339fede529386a21d
robertogriel <roberto@griel.com.br>
2022-05-11 22:04:32 -0300

    Starting product types on Carte

--- Components/Carte/index.tsx
+++ Components/Carte/index.tsx
@@ -1,64 +1,141 @@
-import { useState } from "react";
-import { Burgers } from "../../Types/BurgerType";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Burgers, IngredientByType, IngredientType, Selectedburger } from "../../Types/BurgerType";
+import { formatPrice } from "../../utils/formatPrice";
 import Button from "../Button"
+import { Loading } from "../Loading";
 import { Title } from "../Title"
-
 
 
 export const Carte = () => {
 
-    const [burgers] = useState<Burgers[]>([]);
+	const [burgers, setBurgers] = useState<Burgers[]>([]);
+	const [ingredientType, setIngredientTypes] = useState<IngredientType[]>([]);
+	const [ingredientByType, setIngredientByTypes] = useState<IngredientByType[]>([]);
+	const [selectedBurger, setSelectedBurger] = useState<number>();
+
+	const getBurgers = async () => {
+		const results = await axios.get('/products', {
+			baseURL: process.env.API_URL
+		})
+
+		return results;
+	}
+
+	const getIngredientTypes = async () => {
+		const results = await axios.get('/ingredients/types', {
+			baseURL: process.env.API_URL
+		})
+
+		return results;
+	}
+
+	const getIngredientByTypes = async (id: number) => {
+		const results = await axios.get(`ingredients/by-type/${id}`, {
+			baseURL: process.env.API_URL
+		})
+
+		return results;
+	}
+
+	const setBurger = (id: number) => {
+
+		setSelectedBurger(id)
+
+	}
+
+	useEffect(() => {
+
+		getBurgers()
+			.then(({ data }) => {
+				setBurgers(data);
+			})
+
+		getIngredientTypes()
+			.then(({ data }) => {
+				setIngredientTypes(data);
+			})
+
+	}, []);
+
+	useEffect(() => {
+
+		const ingredients: any = [];
+
+		ingredientType.forEach(async (item) => {
 
 
 
-    return (
-        <>
-        
-        <main>
-            <Title text={<h1>Monte o seu <span>Hburger</span></h1>} />
-            <section>
-                <div className="category" id="burger">
-                    <h2>Escolha seu H-Burger</h2>
-                    <p>Primeiro, escolha seu lanche. Você pode adicionar mais ingredientes depois.</p>
-                    <ul className="burger" style={{ marginBottom: '50px' }}>
-                        <li>
-                            <label data-id="2" className="inputRadio" data-burgername="H-Burger Pro" data-name="H-Burger Pro" data-price="25.5">
-                                <input type="radio" name="burger" id="burger-2" />
-                                <span className="spanRadio"></span>
-                                <h3>H-Burger Pro <span>(Pão, Hamburger, Ovo, Mussarela, Presunto, Alface, Milho, Batata Palha, Bacon e Catupiry)</span></h3>
-                                <div>R$&nbsp;25,50</div>
-                            </label>
-                        </li>
-                        <li>
-                            <label data-id="3" data-burgername="H-Burger Pro" data-name="H-Burger Pro" data-price="25.5">
-                                <input type="radio" name="burger" id="burger-3" />
-                                <span className="spanRadio"></span>
-                                <h3>H-Burger Pro <span>(Pão, Hamburger, Ovo, Mussarela, Presunto, Alface, Milho, Batata Palha, Bacon e Catupiry)</span></h3>
-                                <div>R$&nbsp;25,50</div>
-                            </label>
-                        </li>
-                    </ul>
-                </div>
-                <div className="category hide" id="bread">
-                    <h2>Quer um pão diferente? <small>(escolha apenas um)</small></h2>
-                    <ul className="bread">
+			const result = await getIngredientByTypes(item.id)
+				.then(({ data }) => {
+					// console.log(data)
 
-                    </ul>
-                    <a href="#" className="btnBack" style={{ marginBottom: '50px' }}><span>Voltar pro lanche</span></a>
-                </div>
-                <div className="category hide" id="aditionals">
-                    <h2>Quer turbinar seu lanche? <small>(Você pode escolher a vontade, ou simplesmente avançar)</small></h2>
-                    <ul className="aditionals">
+					return data;
 
-                    </ul>
-                    <a href="#bread" className="btnBack" style={{ marginBottom: '50px' }}><span>Voltar pro pão</span></a>
-                </div>
-            </section>
-        </main>
-        <footer>
-            <Button className="none" value="Escolha seu Lanche" disabled={true} tag={"button"} id="saveBurger" />
-            <h2>R$0,00</h2>
-        </footer>
-        </>
-    )
+				})
+
+			ingredients.push(result);
+
+			setIngredientByTypes(ingredients);
+
+		});
+
+
+
+	}, [ingredientType]);
+
+
+
+	return (
+		<>
+
+			<main>
+				<Title text={<h1>Monte o seu <span>Hburger</span></h1>} />
+				<section>
+					<div className={selectedBurger ? 'category hide' : 'category'} id="burger">
+						<h2>Escolha seu H-Burger</h2>
+						<p>Primeiro, escolha seu lanche. Você pode adicionar mais ingredientes depois.</p>
+						{burgers.length === 0 ? <Loading /> : ''}
+
+
+						{burgers && burgers.map(({ id, name, price, description }) => (
+							<ul className="burger" key={id}>
+								<li>
+									<label data-id="2" className='inputRadio' data-burgername={name} data-name={name} data-price={price}>
+										<input type="radio" name="burger" id={`burger-${id}`} onChange={() => { setBurger(id) }} />
+										<span className="spanRadio"></span>
+										<h3>{name} <span>({description})</span></h3>
+										<div>{formatPrice(price)}</div>
+									</label>
+								</li>
+							</ul>
+						))}
+
+
+					</div>
+					{selectedBurger && <div className="category" id="aditionals">
+						<>
+							<h2>Quer turbinar seu lanche? <small>(Você pode escolher a vontade, ou simplesmente avançar)</small></h2>
+
+							{ingredientType &&
+								ingredientType.map(({ id, name, description }) => (
+									<h3>{name} <p>{description}</p></h3>
+
+
+								))
+							}
+
+							<a href="#bread" className="btnBack" style={{ marginBottom: '50px' }}><span>Voltar para o lanche</span></a>
+						</>
+					</div>}
+
+
+				</section>
+			</main>
+			<footer>
+				<Button className="none" value="Escolha seu Lanche" disabled={true} tag={"button"} id="saveBurger" />
+				<h2>R$0,00</h2>
+			</footer>
+		</>
+	)
 }
--- Types/BurgerType.ts
+++ Types/BurgerType.ts
@@ -1,6 +1,22 @@
+
 export type Burgers = {
     id: number;
     name: string;
     description: string;
     price: number;
+}
+
+export type Selectedburger = {
+    id: number;
+}
+
+export type IngredientType = {
+    id: number;
+    name: string;
+    description: string;
+}
+
+export type IngredientByType = {
+    ingredient: IngredientType;
+    price: string;
 }
