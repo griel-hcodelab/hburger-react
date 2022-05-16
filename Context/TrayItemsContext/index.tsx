dbf0ecb799596021b877986db89d850a457a409c
robertogriel <roberto@griel.com.br>
2022-05-16 15:01:43 -0300

    Removed consoles

--- Context/TrayItemsContext/index.tsx
+++ Context/TrayItemsContext/index.tsx
@@ -52,8 +52,6 @@ export default function TrayItemsProvider({ children }: { children: ReactNode })
 
     useEffect(()=>{
 
-        console.log(trayItems.length)
-
         const sum:number[] = []
 
         trayItems.forEach((item:any)=>{
@@ -87,71 +85,7 @@ export default function TrayItemsProvider({ children }: { children: ReactNode })
         if (aditional) {
             const filtered = trayItems.filter((item:any) => item.id !== burgerId);
             setTrayItems(filtered)
-            console.log(filtered)
         }
-    }
-
-    const oculto = {
-    // const [burger, setBurger] = useState<TrayItemsTypes[]>([]);
-    // const [aditionals, setAditionals] = useState<TrayItemsTypes[]>([]);
-    // const [subtotal, setSubTotal] = useState<number>(0);
-
-    // const [burgerValues, setBurgerValues] = useState<number[]>([])
-    // const [aditionalsValues, setAditionalsValues] = useState<number[]>([])
-
-    // const addBurger = ({ id, name, price }: { id: number; name: string; price: number; }) => {
-
-    //     const newArray = [...burgerValues, Number(price)];
-              
-    //     setBurgerValues(newArray);
-
-    // }
-
-    // useEffect(()=>{
-
-    //     console.log('values',values)
-       
-    //     const sum = values.reduce((a, b) => a + b, 0);
-        
-    //     setSubTotal(sum);
-
-    // },[aditionalValues])
-
-    // const removeBurger = (id: number) => {
-    //     // burger.filter(({ id }) => id !== id);
-
-    // }
-
-    // const addAditionals = ({ id, name, price }: { id: number; name: string; price: number; }) => {
-
-    //     const newAditionals = [...aditionals, { id, name, price }];
-
-    //     setAditionalValues([Number(price), ...values]);
-
-    //     setAditionals(newAditionals);
-
-    // }
-
-    // const removeAditionals = ({id}:{id:number}) => {
-
-    //     const aditional = aditionals.find((item)=> item.id === id);
-
-    //     if (aditional) {
-    //         const filtered = aditionals.filter((item) => item.id !== id);
-
-    //         aditionalValues.splice(aditionalValues.indexOf(Number(aditional.price)), 1);
-
-    //         setAditionalValues([...aditionalValues]);
-
-
-    //         console.log('newvalues',aditionalValues)
-
-
-    //         setAditionals(filtered)
-    //     }
-
-
-    // }
     }
 
     return (
